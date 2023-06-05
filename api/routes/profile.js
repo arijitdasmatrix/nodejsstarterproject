e17cbaa45dba65e8bc57fb6ed0b4ddbb0d23550e
arijitdasmatrix <arijitdas@matrixnmedia.com>
2023-06-05 07:16:09 +0530

    node

--- api/routes/profile.js
+++ api/routes/profile.js
@@ -1,18 +1,50 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const cors = require('cors')
+const profileController = require("../controllers/profile");
+
+var allowlist = ['http://localhost:3000', 'http://localhost:5000']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
 
 
-router.get('/',(req,res,next) => {
+router.get('/', cors(corsOptionsDelegate),(req,res,next) => {
  res.status(200).json({
  message:"handleing get request profile data"
  });
-});
+ });
 
-router.post('/',(req,res,next) => {
-res.status(200).json({
-message:"handleing post request profile data"
-});
-});
+ router.post('/', cors(corsOptionsDelegate),profileController.addNewProfile);
 
+
+ router.get('/:userid', cors(corsOptionsDelegate),(req,res,next) => {
+ const id = req.params.userid;
+ res.status(200).json({
+ id:id
+ });
+ });
+
+ router.patch('/:userid', cors(corsOptionsDelegate),(req,res,next) => {
+ const id = req.params.userid;
+ res.status(200).json({
+ id:id,
+ message:"Update successful"
+ });
+ });
+
+ router.delete('/:userid', cors(corsOptionsDelegate),(req,res,next) => {
+ const id = req.params.userid;
+ res.status(200).json({
+ id:id,
+ message:"Delete successful"
+ });
+ });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 const express = require("express");
 const app = express();
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
 const profileRoutes = require("./api/routes/profile");
+const db = require("./utils/database");
+
+
+
+app.use(morgan('dev'));
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
 
 app.use('/Users',profileRoutes);
+
+app.use((req,res,next) => {
+const error = new Error("404 Not found");
+error.status = 404;
+next(error);
+});
+
+app.use((error,req,res,next ) => {
+res.status(error.status || 500);
+res.json({
+error: {
+  message:error.message  
+}
+}); 
+});
 
 module.exports = app;
